dc78bc984e11c5756d00d2e07ed35126345f23c0
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2026-02-18 12:46:40 -0300

    feat: create parallel menu route + default page

---
+++ app/menu/@modal/(.)[item-id]/page.tsx
@@ -0,0 +1,5 @@
+import { Modal } from "@/app/ui/modal";
+
+export default function Page() {
+  return <Modal>Modal working as expected</Modal>;
+}
---
+++ app/menu/[item-id]/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Hello full page</h1>;
+}
