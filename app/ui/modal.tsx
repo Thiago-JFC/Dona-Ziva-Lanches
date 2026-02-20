324ccec28be74e2c9c4e7d1ada4e6b6840f9a86b
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2026-02-20 16:22:48 -0300

    feat: separate modal button from modal component

--- app/ui/modal.tsx
+++ app/ui/modal.tsx
@@ -2,16 +2,40 @@
 import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
 
-export function Modal({ children }: { children: ReactNode }) {
+/* 
+  // TODO: IMPLEMENT COMPOUND COMPONENT PATTERN 
+*/
+
+export function Modal({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className="fixed top-0 right-0 z-999 flex h-full w-full justify-center bg-black/50">
+      <div
+        className={`h-full w-full max-w-lg overflow-y-scroll rounded-3xl bg-white px-2 py-10 sm:p-8 lg:my-16 lg:h-fit lg:max-w-7xl lg:p-16 ${className}`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export function CloseModalButton({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   const router = useRouter();
 
   return (
-    <div className="fixed top-0 right-0 z-999 flex h-full w-full justify-center bg-black/50">
-      <div className="bg-neutral-200">
-        {children}
-
-        <button onClick={router.back}>close modal</button>
-      </div>
-    </div>
+    <button className={className} onClick={router.back}>
+      {children}
+    </button>
   );
 }
