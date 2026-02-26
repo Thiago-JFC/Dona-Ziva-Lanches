ab178aa2bc3de17ce7827e51b236a6e41b6b68c1
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2026-02-26 17:22:29 -0300

    feat: create parallel cart route

--- app/menu/layout.tsx
+++ app/menu/layout.tsx
@@ -1,14 +1,17 @@
 export default function MenuLayout({
   children,
   modal,
+  sideCartSection,
 }: {
   children: React.ReactNode;
   modal: React.ReactNode;
+  sideCartSection: React.ReactNode;
 }) {
   return (
     <>
       {modal}
       {children}
+      {sideCartSection}
     </>
   );
 }
