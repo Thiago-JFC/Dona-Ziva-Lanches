e4231e7ccbf8dcb6553bad2d398c4b1508abcfa1
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2026-02-28 18:07:47 -0300

    refactor: change dynamic route name to easily refer to its parameter in components

--- app/menu/@modal/(.)item/[id]/page.tsx
+++ app/menu/@modal/(.)item/[id]/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { MenuItem } from "@/app/lib/menu";
+import { CartContext } from "@/app/menu/context/cartContext";
 import { CloseModalButton, Modal } from "@/app/ui/modal";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { use, useContext } from "react";
 
-export default function Page() {
+export default function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const x = use(params);
+  console.log(x.id);
+
   return (
     <Modal className="lg:flex lg:gap-16">
       <Image
@@ -75,14 +87,41 @@ export default function Page() {
         </div>
 
         <div className="flex flex-col gap-4 lg:flex-row">
-          <button className="bg-primary-400 basis-2/3 cursor-pointer rounded-sm py-4 font-bold text-white">
-            Confirmar (total R$ 6,99)
-          </button>
+          <AddItemToCartButton />
+
           <CloseModalButton className="text-secondary-400 border-secondary-400 basis-1/3 cursor-pointer rounded-sm border-2 px-6 py-2">
             Cancelar
           </CloseModalButton>
         </div>
       </div>
     </Modal>
+  );
+}
+
+function AddItemToCartButton() {
+  const mockedItem: MenuItem = {
+    id: "12",
+    title: "Coxinha com requeijão",
+    description:
+      "Coxinha crocante por fora, recheada com frango desfiado e requeijão cremoso. Irresistível docomeço ao fim.",
+    price: 7.99,
+  };
+
+  const { addItem, cart } = useContext(CartContext);
+  const route = useRouter();
+
+  function addItemToCart() {
+    addItem(mockedItem);
+    route.back();
+    console.log(cart.length);
+  }
+
+  return (
+    <button
+      className="bg-primary-400 basis-2/3 cursor-pointer rounded-sm py-4 font-bold text-white"
+      onClick={addItemToCart}
+    >
+      Confirmar (total R$ 6,99)
+    </button>
   );
 }
