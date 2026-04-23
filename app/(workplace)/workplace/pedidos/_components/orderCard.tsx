dae1fddffa029d9c91ecf7773126f086da6c3fef
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2026-04-23 15:22:49 -0300

    feat: order status management page

--- app/(workplace)/workplace/pedidos/_components/orderCard.tsx
+++ app/(workplace)/workplace/pedidos/_components/orderCard.tsx
@@ -1,12 +1,24 @@
 "use client";
-import { Tables } from "@/database.types";
+import { Database, Tables } from "@/database.types";
 import { createClient } from "@/lib/supabase/client";
 import { useEffect, useState } from "react";
 
 type OrderItem = Tables<"order_item">;
+type Order = Database["public"]["Tables"]["order"]["Row"];
 
 export function OrderCard({ orderId }: { orderId: number }) {
-  const [orderitems, setOrderItems] = useState<OrderItem[]>();
+  const supabase = createClient();
+
+  const [orderItems, setOrderItems] = useState<OrderItem[]>();
+
+  async function updateOrderStatus(status: Order["status"], id: Order["id"]) {
+    const { data, error } = await supabase
+      .from("order")
+      .update({ status })
+      .eq("id", id)
+      .select();
+    console.log(data, error);
+  }
 
   useEffect(() => {
     const supabase = createClient();
@@ -32,14 +44,29 @@ export function OrderCard({ orderId }: { orderId: number }) {
   return (
     <li className="bg-light-card w-[25%] rounded-md p-4 shadow">
       <h3 className="mb-1.5 text-xl">Pedido de Mateus</h3>
-      {orderitems?.map((item) => (
+      {orderItems?.map((item) => (
         <div key={item.id}>
           <p className="text-sm text-gray-600">
             {" "}
-            {item.menu_item.title} (R${item.price})
+            {item.menu_item?.title} (R${item.price})
           </p>
         </div>
       ))}
+
+      <h4>Atualizar status para:</h4>
+
+      <select
+        name=""
+        id=""
+        onChange={(e) =>
+          updateOrderStatus(e.target.value as Order["status"], orderId)
+        }
+      >
+        <option value="not started">Aguardando confirmação</option>
+        <option value="preparing">Em preparo</option>
+        <option value="ready">Pronto</option>
+        <option value="canceled">Cancelado</option>
+      </select>
     </li>
   );
 }
--- app/(workplace)/workplace/pedidos/page.tsx
+++ app/(workplace)/workplace/pedidos/page.tsx
@@ -1,99 +1,49 @@
 "use client";
-import { Database } from "@/database.types";
-import { createClient } from "@/lib/supabase/client";
-import { useEffect, useState } from "react";
+import type { Tables } from "@/database.types";
 import { OrderCard } from "./_components/orderCard";
+import { useOrders } from "@/hooks/useOrders";
 
-type Order = Database["public"]["Tables"]["order"]["Row"];
+type OrderStatus = Tables<"order">["status"];
 
 export default function Page() {
-  const [orders, setOrders] = useState<Order[]>([]);
-  const supabase = createClient();
-
-  async function updateOrderStatus(status: Order["status"], id: Order["id"]) {
-    const { data, error } = await supabase
-      .from("order")
-      .update({ status })
-      .eq("id", id)
-      .select();
-    console.log(data, error);
-  }
-
-  /* FETCH ORDERS FOR THE FIRST TIME */
-  useEffect(() => {
-    async function fetchOrders() {
-      const { data, error } = await supabase.from("order").select("*");
-
-      if (data) setOrders(data);
-      console.log(error);
-    }
-
-    fetchOrders();
-  }, []);
-
-  /* SUBSCRIBE ON REAL TIME CHANGES */
-  useEffect(() => {
-    const channel = supabase
-      .channel("orders_channel")
-      .on(
-        "postgres_changes",
-        {
-          event: "*",
-          schema: "public",
-          table: "order",
-        },
-        (payload) => {
-          if (payload.eventType === "UPDATE") {
-            setOrders((prev) =>
-              prev.map((o) =>
-                o.id === payload.new.id ? (payload.new as Order) : o,
-              ),
-            );
-          }
-        },
-      )
-      .subscribe();
-
-    return () => {
-      supabase.removeChannel(channel);
-    };
-  }, []);
+  const { orders } = useOrders();
+  const orderStatus: OrderStatus[] = [
+    "canceled",
+    "not started",
+    "preparing",
+    "ready",
+  ];
 
   return (
     <>
       <main>
-        <section>
-          <h2 className="text-2xl font-bold">Aguardando confirmação</h2>
+        {orderStatus.map((status) => (
+          <section key={status}>
+            <h2 className="text-2xl font-bold">{status}</h2>
 
-          <ul className="flex gap-4">
-            {orders.map((order) => {
-              if (order.status == "not started")
-                return <OrderCard orderId={order.id} key={order.id} />;
-            })}
-          </ul>
-        </section>
+            <ul className="flex gap-4">
+              {orders
+                .filter((order) => order.status == status)
+                .map((order) => (
+                  <OrderCard orderId={order.id} key={order.id} />
+                ))}
+            </ul>
+          </section>
+        ))}
 
-        <section>
-          <h2 className="text-2xl font-bold">Em preparo</h2>
+        {/* {
+          orderStatus.map((status) => (
+            <section key={status}>
+              <h2 className="text-2xl font-bold">{status}</h2>
 
-          <ul className="flex gap-4">
-            {orders.map((order) => {
-              if (order.status == "preparing")
-                return <OrderCard orderId={order.id} key={order.id} />;
-            })}
-          </ul>
-        </section>
-
-        <section>
-          <h2 className="text-2xl font-bold">Pronto pra entrega</h2>
-
-          <ul className="flex gap-4">
-            {orders.map((order) => {
-              if (order.status == "ready")
-                return <OrderCard orderId={order.id} key={order.id} />;
-            })}
-          </ul>
-        </section>
+              <ul className="flex gap-4">
+                {orders.map((order) => {
+                  if (order.status == status)
+                    return <OrderCard orderId={order.id} key={order.id} />;
+                })}
+              </ul>
+            </section>
+        ))} */}
       </main>
     </>
   );
